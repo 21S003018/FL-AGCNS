8fd4efa07a48c3426c7ec64bee6cb8e498b493a6
bozhouchen <bozhouchen@hit.edu.cn>
2021-11-24 05:45:31 +0000

    .

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ code验证:
 python server.py --client 8 --mode test --code 2 11 5 20 7 0 47 2 --dataset Physics
 python client.py --client 8
 2 9 5 8 0 3 7 5
+python server.py --client 3 --mode test --code 1 10 6 5 8 0 12 2 --dataset cora
+python client.py --client 3 --mode test
+graphnas|cora:1 10 6 5 8 0 12 2
 
 random模型:
 python server.py --client 3 --model fl-random --dataset cora
